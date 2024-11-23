652a18098cd686ed427f6469440c9a5dd3d00ebc
MartinKoome <Koomemartin@users.noreply.huggingface.co>
2024-11-23 10:41:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -37,13 +37,13 @@ class Website:
 link_system_prompt = "You are provided with a list of links found on a webpage. \
 You are able to decide which of the links would be most relevant to include in a brochure about the company, \
 such as links to an About page, or a Company page, or Careers/Jobs pages.\n"
+link_system_prompt +="Kindly avoid selecting email links with this: \n mailto:company@gmail.com \n "
 link_system_prompt += "You should respond in JSON as in this example:"
 link_system_prompt += """
 {
     "links": [
         {"type": "about page", "url": "https://full.url/goes/here/about"},
-        {"type": "careers page": "url": "https://another.full.url/careers"},
-        # {"type": "contanct us" : "url" : "mailto:company@gmail.com"}
+        {"type": "careers page": "url": "https://another.full.url/careers"}
     ]
 }
 """
